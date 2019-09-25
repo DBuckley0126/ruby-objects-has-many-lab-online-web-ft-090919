f33290aad26e52989af86cf934077f903ae3a240
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-25 20:24:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,4 +11,11 @@ attr_accessor :name, :posts
     post.author = self
     @posts << post
   end
+
+  def add_post_by_author (title)
+    post = Post.new (title)
+    post.author = self
+    @posts << post
+    
+  end
 end
