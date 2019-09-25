4f528909662d8952bd0efa384c0729d880a11ac6
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-25 20:27:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,16 @@
 class Post
 
+@@all = []
+
+def self.all
+  @@all
+end
+
   attr_accessor :title, :author
 
   def initialize (title)
     @title = title
+    @all << self
   end
 
   def author= (author)
